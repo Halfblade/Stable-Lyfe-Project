cbfe62ca6943cd07d4affd3619f52054d15461eb
Jason Moore <JAMoore2@student.fullsail.edu>
2020-08-06 18:58:58 -0400

    added sign up button with functionality

--- app/src/main/java/com/example/stablelyfe/CreateLoginWithEmail.kt
+++ app/src/main/java/com/example/stablelyfe/CreateLoginWithEmail.kt
@@ -1,11 +1,23 @@
 package com.example.stablelyfe
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class CreateLoginWithEmail : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_login_with_email)
+
+
+        val buttonSignUpWithEmail = findViewById<Button>(R.id.button10)
+
+
+        buttonSignUpWithEmail.setOnClickListener{
+            val intent = Intent(this, StablersHomePage::class.java)
+
+            startActivity(intent)
+        }
     }
 }
