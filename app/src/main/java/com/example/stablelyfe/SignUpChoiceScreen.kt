05d972f8043715f51c335900f9cae1cbad6d94e6
Jason Moore <JAMoore2@student.fullsail.edu>
2020-08-06 18:37:00 -0400

    added functionality to sign up choice email button

--- app/src/main/java/com/example/stablelyfe/SignUpChoiceScreen.kt
+++ app/src/main/java/com/example/stablelyfe/SignUpChoiceScreen.kt
@@ -1,11 +1,22 @@
 package com.example.stablelyfe
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class SignUpChoiceScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up_choice_screen)
+
+        val buttonLoginWithEmail = findViewById<Button>(R.id.button8)
+
+
+        buttonLoginWithEmail.setOnClickListener{
+            val intent = Intent(this, CreateLoginWithEmail::class.java)
+
+            startActivity(intent)
+        }
     }
 }
