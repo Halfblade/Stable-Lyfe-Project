869723bd70f2eb6a6f08e74768a87b2f08029290
Jason Moore <JAMoore2@student.fullsail.edu>
2020-08-08 18:05:20 -0400

    added new activity StablersAddAndRemoveHorses

---
+++ app/src/main/java/com/example/stablelyfe/StablersAddAndRemoveHorses.kt
@@ -0,0 +1,11 @@
+package com.example.stablelyfe
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class StablersAddAndRemoveHorses : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_stablers_add_and_remove_horses)
+    }
+}
