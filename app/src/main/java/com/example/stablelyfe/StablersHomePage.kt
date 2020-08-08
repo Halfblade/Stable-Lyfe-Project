25ab20bf107f1d10cd373b1cb3f538f5a3b8ff54
Jason Moore <JAMoore2@student.fullsail.edu>
2020-08-08 18:07:52 -0400

    added button functionality

--- app/src/main/java/com/example/stablelyfe/StablersHomePage.kt
+++ app/src/main/java/com/example/stablelyfe/StablersHomePage.kt
@@ -12,10 +12,17 @@ class StablersHomePage : AppCompatActivity() {
 
 
         val buttonMyBarn = findViewById<Button>(R.id.button3)
-
+        val buttonAddRemoveHorses = findViewById<Button>(R.id.button4)
 
         buttonMyBarn.setOnClickListener{
             val intent = Intent(this, StablersMyBarnPage::class.java)
+
+            startActivity(intent)
+        }
+
+
+        buttonAddRemoveHorses.setOnClickListener{
+            val intent = Intent(this, StablersAddAndRemoveHorses::class.java)
 
             startActivity(intent)
         }
