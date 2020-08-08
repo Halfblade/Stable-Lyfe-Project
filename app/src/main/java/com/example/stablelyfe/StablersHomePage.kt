e216fc864a3f990e7244b6dcfffa623b045f3271
Jason Moore <JAMoore2@student.fullsail.edu>
2020-08-08 18:02:44 -0400

    added new activity StablersMyBarnPage and button to get to it

--- app/src/main/java/com/example/stablelyfe/StablersHomePage.kt
+++ app/src/main/java/com/example/stablelyfe/StablersHomePage.kt
@@ -1,11 +1,23 @@
 package com.example.stablelyfe
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class StablersHomePage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_stablers_home_page)
+
+
+        val buttonMyBarn = findViewById<Button>(R.id.button3)
+
+
+        buttonMyBarn.setOnClickListener{
+            val intent = Intent(this, StablersMyBarnPage::class.java)
+
+            startActivity(intent)
+        }
     }
 }
