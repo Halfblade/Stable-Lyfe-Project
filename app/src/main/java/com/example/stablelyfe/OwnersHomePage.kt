36792efe0c364235e36b7828140392588134e5e2
Jason Moore <JAMoore2@student.fullsail.edu>
2020-08-10 21:34:59 -0400

    added my horse page and link from OwnersHomePage

--- app/src/main/java/com/example/stablelyfe/OwnersHomePage.kt
+++ app/src/main/java/com/example/stablelyfe/OwnersHomePage.kt
@@ -1,11 +1,23 @@
 package com.example.stablelyfe
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class OwnersHomePage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_owners_home_page)
+
+
+        val buttonMyHorse = findViewById<Button>(R.id.button11)
+
+
+        buttonMyHorse.setOnClickListener {
+            val intent = Intent(this, OwnersMyHorse::class.java)
+
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/example/stablelyfe/OwnersMyHorse.kt
@@ -0,0 +1,11 @@
+package com.example.stablelyfe
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OwnersMyHorse : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_owners_my_horse)
+    }
+}
