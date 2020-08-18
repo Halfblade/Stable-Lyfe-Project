bf34a38e05d5c485a945ad579a3544c837efeb49
Jason Moore <JAMoore2@student.fullsail.edu>
2020-08-18 15:13:07 -0400

    created button for western pleasure training with link

--- app/src/main/java/com/example/stablelyfe/StableLyfeTraining.kt
+++ app/src/main/java/com/example/stablelyfe/StableLyfeTraining.kt
@@ -1,11 +1,23 @@
 package com.example.stablelyfe
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class StableLyfeTraining : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_stable_lyfe_training)
+
+
+        val buttonWesternPleasureTraining = findViewById<Button>(R.id.button21)
+
+
+        buttonWesternPleasureTraining.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.westernhorseuk.com/whuk-blog/western-pleasure-work-out.hk"))
+            startActivity(intent)
+        }
     }
 }
