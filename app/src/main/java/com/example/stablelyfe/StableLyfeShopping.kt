3ed94bb65d97af8567165de8164170f16c1c48e1
Jason Moore <JAMoore2@student.fullsail.edu>
2020-08-18 14:30:51 -0400

    connected the button to amazon link, need to update link

--- app/src/main/java/com/example/stablelyfe/StableLyfeShopping.kt
+++ app/src/main/java/com/example/stablelyfe/StableLyfeShopping.kt
@@ -1,11 +1,23 @@
 package com.example.stablelyfe
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class StableLyfeShopping : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_stable_lyfe_shopping)
+
+        val buttonBrowseSaddles = findViewById<Button>(R.id.button16)
+
+
+
+        buttonBrowseSaddles.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.amazon.com/Equestrian-Saddles/b?ie=UTF8&node=3407621.kh"))
+            startActivity(intent)
+        }
     }
 }
