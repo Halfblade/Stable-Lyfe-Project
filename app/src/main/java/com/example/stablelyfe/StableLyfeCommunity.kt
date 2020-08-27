587707501a7c405bbab6ff4142ec64b4f536a205
Jason Moore <JAMoore2@student.fullsail.edu>
2020-08-27 16:54:37 -0400

    Added menu navi, bottom menu, and icons

--- app/src/main/java/com/example/stablelyfe/StableLyfeCommunity.kt
+++ app/src/main/java/com/example/stablelyfe/StableLyfeCommunity.kt
@@ -3,9 +3,9 @@ package com.example.stablelyfe
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class StableLifeMessages : AppCompatActivity() {
+class StableLyfeCommunity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_stable_life_messages)
+        setContentView(R.layout.activity_stable_lyfe_community)
     }
 }
