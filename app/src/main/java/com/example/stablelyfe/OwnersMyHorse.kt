53357c98caac5869f94852936946013deda88a65
Jason Moore <JAMoore2@student.fullsail.edu>
2020-10-13 17:43:09 -0400

    tried to create fragment with calendar, didn't go well. Fix later

--- app/src/main/java/com/example/stablelyfe/OwnersMyHorse.kt
+++ app/src/main/java/com/example/stablelyfe/OwnersMyHorse.kt
@@ -2,10 +2,25 @@ package com.example.stablelyfe
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.CheckBox
 
 class OwnersMyHorse : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_owners_my_horse)
+
+        val checkboxSelectAll = findViewById<CheckBox>(R.id.checkBox4)
+
+        val buttonAddMyHorse = findViewById<Button>(R.id.button25)
+
+        buttonAddMyHorse.setOnClickListener {
+
+        }
+
+        if (checkboxSelectAll.isActivated){
+
+        }
+
     }
 }
