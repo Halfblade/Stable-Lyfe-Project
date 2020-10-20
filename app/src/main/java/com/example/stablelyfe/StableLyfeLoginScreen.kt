d066fc7b47451ed6c92800ddbf45ebd88a8f332c
Jason Moore <JAMoore2@student.fullsail.edu>
2020-10-20 17:04:01 -0400

    hid action bar on beginning screen

--- app/src/main/java/com/example/stablelyfe/StableLyfeLoginScreen.kt
+++ app/src/main/java/com/example/stablelyfe/StableLyfeLoginScreen.kt
@@ -1,19 +1,22 @@
 package com.example.stablelyfe
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
 import android.widget.Button
 import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
-import kotlinx.android.synthetic.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var auth : FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        try {
+            this.supportActionBar!!.hide()
+        } catch (e: NullPointerException) {
+        }
 
         auth = FirebaseAuth.getInstance()
 
