9f76b60755c5fea0c88d4e5e5b44a501afdecc8a
Jason Moore <JAMoore2@student.fullsail.edu>
2020-10-20 16:30:36 -0400

    created onclick listener for Special Notes

--- app/src/main/java/com/example/stablelyfe/StablersMyBarnPage.kt
+++ app/src/main/java/com/example/stablelyfe/StablersMyBarnPage.kt
@@ -2,10 +2,39 @@ package com.example.stablelyfe
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class StablersMyBarnPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_stablers_my_barn_page)
+
+
+
+        val buttonSpecialNotes1 = findViewById<Button>(R.id.button26)
+        val buttonSpecialNotes2 = findViewById<Button>(R.id.button28)
+        val buttonSpecialNotes3 = findViewById<Button>(R.id.button29)
+        val buttonSpecialNotes4 = findViewById<Button>(R.id.button31)
+        val buttonSpecialNotes5 = findViewById<Button>(R.id.button32)
+
+        buttonSpecialNotes1.setOnClickListener {
+            
+        }
+
+        buttonSpecialNotes2.setOnClickListener {
+
+        }
+
+        buttonSpecialNotes3.setOnClickListener {
+
+        }
+        buttonSpecialNotes4.setOnClickListener {
+
+        }
+        buttonSpecialNotes5.setOnClickListener {
+
+        }
     }
+
+
 }
